a47b9a83e77e66d2b74b59b3d0ff45f445de49c3
PedroPinMag <pedropinheiromagalhaes@gmail.com>
2022-09-06 18:31:06 -0300

    Expanded UI options

--- Bot/UserInterface.py
+++ Bot/UserInterface.py
@@ -32,8 +32,74 @@ class UI:
         self.agent = str(agent)
         self.console = Console()
 
+    def info(self, msg):
+        msg = str(msg)
+        msg_style = "white"
+        msg = f"{get_formatted_prefix(f_time=get_formatted_time(), f_agent=self.agent, f_type='INFO')} {msg}"
+        self.console.print(msg, style=msg_style)
+
     def error(self, msg):
         msg = str(msg)
+        msg_style = "red"
         msg = f"{get_formatted_prefix(f_time=get_formatted_time(), f_agent=self.agent, f_type='ERROR')} {msg}"
-        self.console.print(msg)
+        self.console.print(msg, style=msg_style)
 
+    def warn(self, msg):
+        msg = str(msg)
+        msg_style = "yellow"
+        msg = f"{get_formatted_prefix(f_time=get_formatted_time(), f_agent=self.agent, f_type='WARN')} {msg}"
+        self.console.print(msg, style=msg_style)
+
+    def custom_msg(self, msg=None, color=None, tag=None):
+        if msg is not None and color is not None and tag is not None:
+            msg = str(msg)
+            msg_style = str(color)
+            msg = f"{get_formatted_prefix(f_time=get_formatted_time(), f_agent=self.agent, f_type=tag)} {msg}"
+            self.console.print(msg, style=msg_style)
+        else:
+            self.warn("Arguments not defined in custom msg")
+
+    def input(self, input_type="string", default=None, question="Undefined question"):
+        if input_type == "string":
+            self.custom_msg(msg=question, color="white", tag="STR INPUT")
+            try:
+                entry = str(input(""))
+            except Exception as e:
+                ui.warn(e)
+                return default
+            else:
+                return entry
+
+        elif input_type == "int":
+            self.custom_msg(msg=question, color="white", tag="INT INPUT")
+            try:
+                entry = int(input(""))
+            except Exception as e:
+                ui.warn(e)
+                return default
+            else:
+                return entry
+
+        elif input_type == "float":
+            self.custom_msg(msg=question, color="white", tag="FLOAT INPUT")
+            try:
+                entry = float(input(""))
+            except Exception as e:
+                ui.warn(e)
+                return default
+            else:
+                return entry
+
+        elif input_type == "bool":
+            self.custom_msg(msg=question, color="white", tag="FLOAT INPUT")
+            try:
+                entry = bool(input(""))
+            except Exception as e:
+                ui.warn(e)
+                return default
+            else:
+                return entry
+        else:
+            self.warn("Undefined input type")
+
+        return None
